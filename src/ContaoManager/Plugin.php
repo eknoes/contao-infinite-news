bad6ff7f19b66e371e83d8647853825b608175d2
Sönke <me@eknoes.de>
2019-09-09 22:13:54 +0200

    Remove Routing

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -20,7 +20,7 @@ use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouteCollection;
 
-class Plugin implements BundlePluginInterface, RoutingPluginInterface
+class Plugin implements BundlePluginInterface
 {
     /**
      * {@inheritdoc}
@@ -33,15 +33,4 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
         ];
     }
 
-    /**
-     * Returns a collection of routes for this bundle.
-     *
-     * @return RouteCollection|null
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
-    {
-                return $resolver
-                    ->resolve(__DIR__.'/../Resources/config/routing.yml')
-                ->load(__DIR__.'/../Resources/config/routing.yml');
-    }
 }
