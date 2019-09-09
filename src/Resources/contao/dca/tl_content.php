e8e3fb99e5eb2a2c0d5dbc6d5fbfd1b960a6e535
Sönke <me@eknoes.de>
2019-09-09 22:46:24 +0200

    Fix imports

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_DCA'][$strName]['fields']['inf_news_module_id'] = array(
+$GLOBALS['TL_DCA']['tl_content']['fields']['inf_news_module_id'] = array(
     'label' => &$GLOBALS['TL_LANG']['tl_content']['inf_news_module_id'],
     'sql' => 'SMALLINT',
     'inputType' => 'select',
@@ -8,7 +8,7 @@ $GLOBALS['TL_DCA'][$strName]['fields']['inf_news_module_id'] = array(
     'eval' => array("multiple" => false)
 );
 
-$GLOBALS['TL_DCA'][$strName]['fields']['inf_news_fallback_module'] = array(
+$GLOBALS['TL_DCA']['tl_content']['fields']['inf_news_fallback_module'] = array(
     'label' => &$GLOBALS['TL_LANG']['tl_content']['inf_news_fallback_module'],
     'sql' => 'SMALLINT',
     'inputType' => 'select',
@@ -16,6 +16,6 @@ $GLOBALS['TL_DCA'][$strName]['fields']['inf_news_fallback_module'] = array(
     'eval' => array("multiple" => false)
 );
 
-$GLOBALS['TL_DCA'][$strName]['palettes']['infiniteNews'] = "{type_legend},type;headline;inf_news_module_id,inf_news_fallback_module";
+$GLOBALS['TL_DCA']['tl_content']['palettes']['infiniteNews'] = "{type_legend},type;headline;inf_news_module_id,inf_news_fallback_module";
 
 ?>
